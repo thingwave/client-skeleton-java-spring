ad7e0b4300dfaac38027384d385f3d60937bff81
Gabor Majoros <gabormajo@yahoo.com>
2019-10-09 12:12:47 +0200

    started to implement client-skeleton-subscriber

---
+++ client-skeleton-subscriber/src/main/java/eu/arrowhead/client/skeleton/subscriber/SubscriberMain.java
@@ -0,0 +1,20 @@
+package eu.arrowhead.client.skeleton.subscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import eu.arrowhead.common.CommonConstants;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {CommonConstants.BASE_PACKAGE}) //TODO: add custom packages if any
+public class SubscriberMain {
+
+	//=================================================================================================
+	// methods
+
+	//-------------------------------------------------------------------------------------------------
+	public static void main(final String[] args) {
+		SpringApplication.run(SubscriberMain.class, args);
+	}	
+}
