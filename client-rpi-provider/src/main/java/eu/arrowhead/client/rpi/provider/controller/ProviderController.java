e8745d5b677bafa9c9e4765a8eb38cb3cefddfdc
Jens Eliasson <jens.eliasson@gmail.com>
2020-07-16 10:01:04 +0200

    LED control now works (no REST interfaces yet)

--- client-rpi-provider/src/main/java/eu/arrowhead/client/rpi/provider/controller/ProviderController.java
+++ client-rpi-provider/src/main/java/eu/arrowhead/client/rpi/provider/controller/ProviderController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import eu.arrowhead.common.CommonConstants;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
 @RestController
 public class ProviderController {
 	
@@ -12,6 +18,16 @@ public class ProviderController {
 	// members
 
 	//TODO: add your variables here
+	private GpioController gpio = null;
+        private GpioPinDigitalOutput pin = null;
+
+	public ProviderController() {
+		gpio = GpioFactory.getInstance();
+                pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "GPIO1", PinState.HIGH);
+
+                pin.setShutdownOptions(true, PinState.LOW);
+
+	}
 
 	//=================================================================================================
 	// methods
